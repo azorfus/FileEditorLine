9ff0f1213989894c755dee752c706ac8446971a6
azorfus <72391760+azorfus@users.noreply.github.com>
2021-03-28 00:01:36 +0530

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,7 +1,7 @@
 @echo off
 set /p option="[1] Release [2] Debug ~$ "
-if %option% == 1 (gcc main.c -o read)
-if %option% == 2 (gcc -Wall -Wextra main.c -o read)
+if %option% == 1 (gcc main.c -o azr)
+if %option% == 2 (gcc -Wall -Wextra main.c -o azr)
 if %option% GTR 2 (echo Error)
 if %option% LSS 1 (echo Error)
 @echo on
